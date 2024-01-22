603751df301368ec6cbc929975dd6e14218013b5
jinhye <jinh2376@gmail.com>
2024-01-22 14:10:52 +0900

    feat: kakao share api

--- src/pages/GroupShare.tsx
+++ src/pages/GroupShare.tsx
@@ -21,6 +21,44 @@ const MainText = styled.div`
 const GroupShare = (): ReactNode => {
   const { pop } = useMainFlow();
 
+  const shareKakao = () => {
+    // window.Kakao.Link.sendCustom({
+    //   templateId: 103292, // 내가 만든 템플릿 아이디
+    //   templateArgs: {
+    //     title: '제목 영역입니다.',
+    //     description: '설명 영역입니다.',
+    //   },
+    // });
+    window.Kakao.Share.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: '그룹 이름',
+        description: '그룹에 대한 설명을 쓰는 곳입니다. 근데 여기에 그룹 사진이 없으면 어떻게 되죠',
+        imageUrl: 'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+          webUrl: 'https://jmt-frontend-ad7b8.web.app/group-detail/1/',
+        },
+      },
+      buttons: [
+        {
+          title: '웹으로 이동',
+          link: {
+            mobileWebUrl: 'https://developers.kakao.com',
+            webUrl: 'https://jmt-frontend-ad7b8.web.app/group-detail/1/',
+          },
+        },
+        {
+          title: '앱으로 이동',
+          link: {
+            mobileWebUrl: 'https://developers.kakao.com',
+            webUrl: 'https://jmt-frontend-ad7b8.web.app/group-detail/1/',
+          },
+        },
+      ],
+    });
+  };
+
   const copyToClipboard = (text: string) => {
     const textarea = document.createElement('textarea');
     textarea.value = text;
@@ -65,7 +103,8 @@ const GroupShare = (): ReactNode => {
         서로의 맛집을 탐방하세요!
       </MainText>
       <ImageBox width={'100%'} height={'300px'} radius_px={0} />
-      <button>카톡으로 공유하기</button>
+
+      <button onClick={shareKakao}>카톡으로 공유하기</button>
       <button onClick={linkShare}>그룹 링크 공유하기</button>
     </AppScreen>
   );
